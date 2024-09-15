9fac70af61ebfcdce30f51432646c7590b69cd7b
AndiappanAR <andiappan.ravi@omnia.ae>
2024-09-15 18:53:46 +0400

    Trigger build

--- nextjs/src/components/OMNIA/SessionsGrid.tsx
+++ nextjs/src/components/OMNIA/SessionsGrid.tsx
@@ -211,7 +211,7 @@ const SessionsGrid = (props: SessionsGridProps): JSX.Element => {
               />
               <button
                 onClick={handleSubmit}
-                className="rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                className="rounded-lg hh bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
                 Search
               </button>
